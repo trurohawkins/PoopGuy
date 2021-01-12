d530947c2168a4d773699ffb0e080edaddd79670
trurohawkins <trubhawkins@gmail.com>
2021-01-12 14:18:23 -0500

    examples for making dynamic arrays

---
+++ exMain.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "poopFunc.c"
+
+int main() {
+	//int *arr = calloc(10, sizeof(int));
+	int *arr = makeArray(10);
+	printf("my pointer arr is at location %p, and its value is %i\n", arr, *arr);
+	int num = 420;
+	int *ptr = &num;
+	printf("my int num is %i at %p, and my pointer is %p at %p\n", *ptr, &num, ptr, &ptr);
+	arr[3] = 56;// arr[3] == *(arr + 3)
+	printf("arr[3] = %i and *(arr + 3) = %i\n", arr[3], *(arr + 3));
+	return 0;
+}
