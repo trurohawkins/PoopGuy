2346875c78f1925497b99ad050369ceeab723000
trurohawkins <trubhawkins@gmail.com>
2021-02-18 18:54:06 -0500

    main.c is back!

---
+++ main.c
@@ -0,0 +1,71 @@
+#include "form/Form.h"
+
+Player *poopGuy;
+void update(int value);
+void keyDown(unsigned char, int, int);
+void keyUp(unsigned char, int, int);
+void exitGame();
+
+int main(int argc, char **argv) {
+	srand(time(NULL));
+	initDirections();
+	int worldX = 100;
+	int worldY = 50;
+	int windowX = 500;
+	int windowY = 500;
+	makeWorld(worldX, worldY);
+	dirtFloor(3);
+	atexit(exitGame);
+	poopGuy = makePlayer();
+	placeForm(4, 49, poopGuy->me->body);
+	setCenter(poopGuy->me->body->pos);
+
+	Actor *rock = makeActor(makeForm(0.3, 0.3, 0.3));
+	Action *move =  makeMove();
+	addAction(rock, move);
+	addAction(rock, makeGravity(move->vars));
+	placeForm(13, 40, rock->body);
+
+	makeActorList();
+	addActor(poopGuy->me);
+	//addActor(rock);
+	
+//	stomachStuff(poopGuy->me->body, poopGuy->eatPoop);
+//	eatPooVar *ep = (eatPooVar*)(poopGuy->eatPoop->vars);
+//	ep->pooping = 1;
+
+	initializeGLUT(argc, argv, windowX, windowY);
+	glutIgnoreKeyRepeat(1);	
+	glutDisplayFunc(drawWorld);	
+	glutKeyboardFunc(keyDown);
+	glutKeyboardUpFunc(keyUp);
+	glutTimerFunc(25, update, 0);
+	glutMainLoop();	
+	
+	return 0;
+}
+
+void keyDown(unsigned char key, int mx, int my) {
+	if (key == 27) {
+		glutLeaveMainLoop();
+	}
+	keyPressPlayer(poopGuy, key);
+}
+
+void keyUp(unsigned char key, int mx, int my) {
+	keyReleasePlayer(poopGuy, key);
+}
+
+void update(int value) {
+	actorListDo();
+	setCenter(poopGuy->me->body->pos);
+	glutPostRedisplay();
+	glutTimerFunc(25, update, 0);
+}
+void exitGame() {
+	printf("coochy");
+	deleteWorld();
+	deletePlayer(poopGuy);
+	deleteActorList();
+	freeDirections();
+}
