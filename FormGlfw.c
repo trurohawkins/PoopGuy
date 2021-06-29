dfd731fe822262d1480112621ef24cd2f031710e
trurohawkins <trubhawkins@gmail.com>
2021-06-28 22:27:17 -0400

    changed background color

--- FormGlfw.c
+++ FormGlfw.c
@@ -41,7 +41,7 @@ void updateLoop() {
 		glUniformMatrix4fv(rMat, 1, GL_TRUE, matrix);
 		World *w = getWorld();
 		while(!glfwWindowShouldClose(screen->window)) {
-			glClearColor(0.0, 0.4, 0.0, 1.0);
+			glClearColor(0.1, 0.2, 0.4, 1.0);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 			//glDrawArrays(GL_TRIANGLES, 0, 6);
 			if (gridOn) {
